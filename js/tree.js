032f9069bd8aa9236b6193d0178fc37f33fbee12
Luc Vachon <lvachon@gmail.com>
2020-02-02 20:39:24 -0500

    instanced geom is super fast!

--- js/tree.js
+++ js/tree.js
@@ -1,34 +1,48 @@
 function Tree(){
-	this.geom = new THREE.BufferGeometry();
 	
-	const verts=[
-	-3,-3, 0,
-	3 ,-3, 0,
-	0 , 3, 0,
-	0 , 0, 12
+	this.verts=[
+	-6,-6, 0,
+	6 ,-6, 0,
+	0 , 6, 0,
+	0 , 0, 24
 	];
-	const colors=[
+	this.colors=[
 	0, 0.25, 0,
 	0, 0.25, 0,
 	0, 0.25, 0,
 	0.25, 0.50, 0.25
 	];
-	const indexes=[
+	this.indexes=[
 	0,1,2,
 	2,0,3,
 	0,1,3,
 	1,2,3
 	];
+	this.offsets=[];
 
-	this.geom.setAttribute('position',new THREE.Float32BufferAttribute(verts,3));
-	this.geom.setAttribute('color',new THREE.Float32BufferAttribute(colors,3));
-	this.geom.setIndex(indexes);
-	this.geom.computeVertexNormals();
-	this.geom.computeFaceNormals();
-	this.mat = new THREE.MeshPhongMaterial({
-		side: THREE.DoubleSide,
-		vertexColors: THREE.VertexColors
-	});
+	this.init = () =>{
+		this.geom = new THREE.InstancedBufferGeometry();
+		this.geom.addAttribute('offset', new THREE.InstancedBufferAttribute(new Float32Array(this.offsets),3));
+		this.geom.setAttribute('position',new THREE.Float32BufferAttribute(this.verts,3));
+		this.geom.setAttribute('color',new THREE.Float32BufferAttribute(this.colors,3));
+		this.geom.setIndex(this.indexes);
+		this.geom.computeVertexNormals();
+		this.geom.computeFaceNormals();
+		this.mat = new THREE.MeshPhongMaterial({
+			side: THREE.DoubleSide,
+			vertexColors: THREE.VertexColors
+		});
+		this.mat.onBeforeCompile = function ( shader ) {
+		    shader.vertexShader = 'attribute vec3 offset;\n' + shader.vertexShader;
+		    shader.vertexShader = shader.vertexShader.replace( '#include <begin_vertex>',
+		       [
+		         'vec3 transformed = vec3( position + offset );',
+		       ].join( '\n' )
+		     );
+		     materialShader = shader;
+		};
 
-	this.mesh = new THREE.Mesh(this.geom, this.mat);
+		this.mesh = new THREE.Mesh(this.geom, this.mat);
+		this.mesh.frustumCulled = false;
+	}
 }
