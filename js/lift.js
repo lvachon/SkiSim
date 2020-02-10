1a0e8422d3c992884b7c5b41c366136a1bfd1ce0
lvachon <lvachon@gmail.com>
2020-02-09 21:31:02 -0500

    Bugfixes inc. lift tower orientation and position calculation trig

--- js/lift.js
+++ js/lift.js
@@ -18,28 +18,27 @@ function Lift(startX, startY, terrain){
     this.setEnd = (endX, endY)=>{
         this.endX = endX;
         this.endY = endY;
-        this.yaw = Math.atan2(this.endY-this.startY, this.endX-this.startX)+Math.PI/2;
-        this.baseHouse.mesh.rotation.z=this.yaw;
+        this.yaw = Math.PI-Math.atan2(this.endY-this.startY, this.endX-this.startX);
+        this.baseHouse.mesh.rotation.z=this.yaw+Math.PI/2;
         let meshPosition = this.terrain.gridToMesh(this.endX, this.endY);
         this.topHouse.mesh.position.x = meshPosition.x;
         this.topHouse.mesh.position.y = meshPosition.y;
         this.topHouse.mesh.position.z = meshPosition.z;
-        this.topHouse.mesh.rotation.z = this.yaw+Math.PI;
-        this.towers.mesh.rotation.z = this.yaw;
-        meshPosition = this.terrain.gridToMesh(this.startX+ Math.cos(this.yaw)*this.distancePerTower, this.startY+Math.sin(this.yaw)*this.distancePerTower);
+        this.topHouse.mesh.rotation.z = this.yaw-Math.PI/2;
+        this.towers.mesh.rotation.z = this.yaw+Math.PI/2;
+        meshPosition = this.terrain.gridToMesh(this.startX, this.startY);
         this.towers.mesh.position.x = meshPosition.x;
         this.towers.mesh.position.y = meshPosition.y;
         this.towers.mesh.position.z = meshPosition.z;
         const offsets = [];
         const dist = Math.sqrt((this.endX-this.startX)*(this.endX-this.startX)+(this.endY-this.startY)*(this.endY-this.startY));
-        console.log(meshPosition);
         for(let i=1;i<dist-this.distancePerTower;i+=this.distancePerTower){
             const nextPosition = this.terrain.gridToMesh(
-                this.startX+ Math.cos(this.yaw-Math.PI/2)*this.distancePerTower*i,
-                this.startY+ Math.sin(this.yaw-Math.PI/2)*this.distancePerTower*i
+                this.startX- Math.cos(this.yaw)*this.distancePerTower*i,
+                this.startY+ Math.sin(this.yaw)*this.distancePerTower*i
             );
-            console.log(nextPosition);
-            offsets.push(nextPosition.x-meshPosition.x,nextPosition.y-meshPosition.y, nextPosition.z-meshPosition.z);
+            const d2 = Math.sqrt((meshPosition.x-nextPosition.x)*(meshPosition.x-nextPosition.x)+(meshPosition.y-nextPosition.y)*(meshPosition.y-nextPosition.y));
+            offsets.push(0,d2, nextPosition.z-meshPosition.z);
         }
         this.towers.geom.setAttribute('offset',new THREE.InstancedBufferAttribute(new Float32Array(offsets),3));
         this.towers.mesh.visible=true;
