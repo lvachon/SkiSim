9b1237e58fd9349f22d07593a53deaae7d876d47
lvachon <lvachon@gmail.com>
2020-03-02 00:20:27 -0500

    Remove commented code

--- js/rider.js
+++ js/rider.js
@@ -24,64 +24,6 @@ function Rider(gridX,gridY,terrain){
         'LIFT':'RIDE',
         'RIDE':'IDLE'
     };
-    /*
-    this.verts = [
-        0, 0, -1,
-        0.7236, -0.52572, -0.447215,
-        -0.276385, -0.85064, -0.447215,
-        -0.894425, 0, -0.447215,
-        -0.276385, 0.85064, -0.447215,
-        0.7236, 0.52572, -0.447215,
-        0.276385, -0.85064, 0.447215,
-        -0.7236, -0.52572, 0.447215,
-        -0.7236, 0.52572, 0.447215,
-        0.276385, 0.85064, 0.447215,
-        0.894425, 0, 0.447215,
-        0, 0, 1
-    ].map(z=>this.radius*z);
-
-    this.indicies = [
-        0, 1, 2,
-        1, 0, 5,
-        0, 2, 3,
-        0, 3, 4,
-        0, 4, 5,
-        1, 5, 10,
-        2, 1, 6,
-        3, 2, 7,
-        4, 3, 8,
-        5, 4, 9,
-        1, 10, 6,
-        2, 6, 7,
-        3, 7, 8,
-        4, 8, 9,
-        5, 9, 10,
-        6, 10, 11,
-        7, 6, 11,
-        8, 7, 11,
-        9, 8, 11,
-        10, 9, 11
-    ];
-
-    this.colors = [];
-    for(let i=0;i<this.verts.length;i+=3){
-        this.colors.push(0,0,1);
-    }
-
-    this.geom = new THREE.BufferGeometry();
-    this.geom.setIndex(this.indicies);
-    this.geom.setAttribute('position',new THREE.Float32BufferAttribute(this.verts,3));
-    this.geom.setAttribute('color',new THREE.Float32BufferAttribute(this.colors,3));
-    this.geom.computeVertexNormals();
-    this.geom.computeFaceNormals();
-    this.mat = new THREE.MeshPhongMaterial({
-        side: THREE.DoubleSide,
-        vertexColors: THREE.VertexColors
-    });
-    this.mesh = new THREE.Mesh(this.geom, this.mat);
-    */
-
-
 
     this.meshPosition = ()=>{
         return new THREE.Vector3(
